c371eb72dd7f63e1e01ad33b1357070ab18b00e6
nedoratic <nedoratic@gmail.com>
2024-04-16 23:29:28 +0200

    main component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
+import Main from './components/Main';
 import './App.css';
 
 const App = () => {
 	return (
 		<>
 			<Navbar />
+			<Main />
 		</>
 	);
 };
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+	return (
+		<main>
+			<h1>Main content goes here</h1>
+		</main>
+	);
+};
+
+export default Main;
