26e40887776c4b2895c9de1d670db692821ab81c
nedoratic <nedoratic@gmail.com>
2024-04-18 22:38:54 +0200

    navbar content edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 const Navbar = () => {
 	return (
-		<>
-			<h1>Navbar goes here</h1>
-		</>
+		<nav className='navbar'>
+			<div className='navbar-logo'>
+				<img className='navbar-logo-image' src='./src/assets/react.svg' alt='React logo' />
+				<h2 className='navbar-logo-text'>React Facts</h2>
+			</div>
+			<h3 className='navbar-title'>React Course - Project 1</h3>
+		</nav>
 	);
 };
 
