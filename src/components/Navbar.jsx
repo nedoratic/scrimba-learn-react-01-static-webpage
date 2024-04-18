4f229839004a20fe50c357fbb81b86462d2f87d4
nedoratic <nedoratic@gmail.com>
2024-04-18 22:52:51 +0200

    navbar styles added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
 		<nav className='navbar'>
 			<div className='navbar-logo'>
 				<img className='navbar-logo-image' src='./src/assets/react.svg' alt='React logo' />
-				<h2 className='navbar-logo-text'>React Facts</h2>
+				<h2 className='navbar-logo-text'>ReactFacts</h2>
 			</div>
 			<h3 className='navbar-title'>React Course - Project 1</h3>
 		</nav>
