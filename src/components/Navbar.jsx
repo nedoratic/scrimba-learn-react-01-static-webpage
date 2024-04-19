c166b8f357a33f829195791c369521d940c224a3
nedoratic <nedoratic@gmail.com>
2024-04-19 21:55:41 +0200

    img src edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import ReactLogo from '../assets/react.svg';
+
 const Navbar = () => {
 	return (
 		<nav className='navbar'>
 			<div className='navbar-logo'>
-				<img className='navbar-logo-image' src='src/assets/react.svg' alt='React logo' />
+				<img className='navbar-logo-image' src={ReactLogo} alt='React logo' />
 				<h2 className='navbar-logo-text'>ReactFacts</h2>
 			</div>
 			<h3 className='navbar-title'>React Course - Project 1</h3>
